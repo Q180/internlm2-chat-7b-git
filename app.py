0582aa686f13a5a39b8b8635c65c526afa24c092
GMQ <102200818+Q180@users.noreply.github.com>
2024-06-05 13:55:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './course4'
+base_path = '/home/xlab-app-center/course4'
 os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/course4.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
